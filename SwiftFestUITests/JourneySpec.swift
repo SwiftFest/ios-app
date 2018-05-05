834ff7fd1f4ef8d40d203cdedc6339d22e729f31
Sean Olszewski <sean.r.olszewski@gmail.com>
2018-05-04 21:22:09 -0400

    A user has a way to navigate all the content in-app (#20)
    [finish #20]
     - Adds tab bar controller that links the agenda to the speakers 
     - Adds a test for viewing more information about a speaker and their session

--- SwiftFestUITests/JourneySpec.swift
+++ SwiftFestUITests/JourneySpec.swift
@@ -34,12 +34,44 @@ class JourneySpec: QuickSpec {
 
             }
             
-            they("a user can tap a bar button item to view the code of conduct") {
+            they("can tap a bar button item to view the code of conduct") {
                 
                 app.buttons["codeOfConductButton"].tap()
                 sleep(3)
                 expect(app.links["Conduct"].exists).to(beTrue())
                 
+            }
+            
+            they("can navigate around the app using a tab bar") {
+
+                let tabBar = app.tabBars.element(boundBy: 0)
+                let agendaButton = tabBar.buttons.element(boundBy: 0)
+                let speakersListButton = tabBar.buttons.element(boundBy: 1)
+
+                expect(app.staticTexts["Session Name 1"].exists).to(beTrue())
+
+                speakersListButton.tap()
+                
+                expect(app.staticTexts["Session Name 1"].exists).to(beFalse())
+                expect(app.staticTexts["Accidentally Famous"].exists).to(beTrue())
+                
+                agendaButton.tap()
+                
+                expect(app.staticTexts["Session Name 1"].exists).to(beTrue())
+                expect(app.staticTexts["Accidentally Famous"].exists).to(beFalse())
+
+            }
+            
+            they("can get more info about a speaker by tapping the title of their session") {
+
+                let tabBar = app.tabBars.element(boundBy: 0)
+                let speakersListButton = tabBar.buttons.element(boundBy: 1)
+
+                speakersListButton.tap()
+                
+                app.staticTexts["Accidentally Famous"].tap()
+                
+                expect(app.staticTexts["Susan Bennett"].exists).to(beTrue())
             }
         }
     }
