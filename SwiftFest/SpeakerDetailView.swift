3e7430ee626dafed3a99d9ecbc0ad1d729d67b7e
Bryan Ryczek <bryanryczek@gmail.com>
2018-05-12 16:17:27 -0400

    - a user should be able to access speaker's social information
    - added logic to call url associated with social button on SpeakerDetailView

--- SwiftFest/SpeakerDetailView.swift
+++ SwiftFest/SpeakerDetailView.swift
@@ -10,13 +10,15 @@ import UIKit
 
 class SpeakerDetailView: UIView {
 
-    var speaker : Speaker?
+    var speaker: Speaker?
     
     @IBOutlet weak var speakerImageView: UIImageView!
     @IBOutlet weak var speakerNameLabel: UILabel!
     @IBOutlet weak var speakerTitleLabel: UILabel!
     @IBOutlet weak var speakerBioLabel: UILabel!
     @IBOutlet weak var socialStackView: UIStackView!
+    
+    var socialUrls: [URL] = []
     
     func uiSetup() {
         speakerImageView.layer.cornerRadius = speakerImageView.frame.height / 2
@@ -34,12 +36,23 @@ class SpeakerDetailView: UIView {
     
     func generateSocialButtonsForSpeaker(_ speaker: Speaker) {
         guard let socialResults = speaker.social else { return }
-        for social in socialResults {
+        for (index, social) in socialResults.enumerated() {
             let socialButton = UIButton(frame: CGRect(x: 0, y: 0, width: 40.0, height: 40.0))
+            socialButton.tag = index
+            if let url = URL(string: social.link) {
+                socialUrls.append(url)
+            }
             let socialImage = UIImage(named: (social.socialType?.rawValue)!)
             socialButton.setImage(socialImage, for: .normal)
+            socialButton.addTarget(self, action: #selector(openSocialUrl), for: .touchUpInside)
             socialStackView.addArrangedSubview(socialButton)
         }
+    }
+    
+    @objc func openSocialUrl(sender: UIButton) {
+        guard socialUrls.count >= sender.tag else { return }
+        let url = socialUrls[sender.tag]
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
     
 }
