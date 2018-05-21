45092cec94c7f55b2eca59fbbf31af8d15712093
Bryan Ryczek <bryanryczek@gmail.com>
2018-05-21 16:47:42 -0400

    -uitab bar cleanup
    - removed code of conduct button from uinavbar
    - added icons for sponsors and info
    - named sponsors and info tabs
    - tinted tab bar icons to be white when highlighted

--- SwiftFest/AgendaViewController.swift
+++ SwiftFest/AgendaViewController.swift
@@ -2,7 +2,6 @@ import UIKit
 
 class AgendaViewController: BaseViewController {
     
-    @IBOutlet weak var codeOfConductButton: UIBarButtonItem!
     @IBOutlet weak var agendaTableView: UITableView!
     
     let agendaTableViewManager: UITableViewDelegate & UITableViewDataSource = TableViewManager(agenda: AppDataController().fetchAgenda(),
@@ -13,7 +12,6 @@ class AgendaViewController: BaseViewController {
         title = L10n.Screen.Agenda.title
         agendaTableView.dataSource = agendaTableViewManager
         agendaTableView.delegate = agendaTableViewManager
-        codeOfConductButton.accessibilityIdentifier = "codeOfConductButton"
     }
 }
 
