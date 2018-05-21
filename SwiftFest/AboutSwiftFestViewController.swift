4b76bfa72ae5b3ad7d6fc0e589beedf545bb0667
Bryan Ryczek <bryanryczek@gmail.com>
2018-05-21 17:07:33 -0400

    - info view controller setup
    - added tableview showing cells "code of conduct" and "about swiftFest"
    - added appropriate segues when user clicks on cells
    - changed some nav bar titles
    - added AboutSwiftFestViewController

---
+++ SwiftFest/AboutSwiftFestViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AboutSwiftFestViewController.swift
+//  SwiftFest
+//
+//  Created by Bryan Ryczek on 5/21/18.
+//  Copyright © 2018 Sean Olszewski. All rights reserved.
+//
+
+import UIKit
+
+class AboutSwiftFestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
