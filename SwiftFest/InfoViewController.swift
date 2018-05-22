24f26d9909a898356464461b002abde8847e3514
Bryan Ryczek <bryanryczek@gmail.com>
2018-05-21 21:58:45 -0400

    Feature/bryanryczek/info view controller (#29)
    * - info view controller setup
    
    - added tableview showing cells "code of conduct" and "about swiftFest"
    - added appropriate segues when user clicks on cells
    - changed some nav bar titles
    - added AboutSwiftFestViewController
    
    * code cleanup.

--- SwiftFest/InfoViewController.swift
+++ SwiftFest/InfoViewController.swift
@@ -1,11 +1,41 @@
+import SafariServices
 import UIKit
 
 class InfoViewController: BaseViewController {
 
+    let infoItems = ["Code of Conduct", "About SwiftFest"]
+    
+    private let codeOfConductURL = URL(string: "http://swiftfest.io/code-of-conduct/")!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+}
 
+extension InfoViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return infoItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "InfoCell")
+        cell.textLabel?.text = infoItems[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch infoItems[indexPath.row] {
+        case "Code of Conduct":
+            let safariBrowser = SFSafariViewController(url: codeOfConductURL)
+            present(safariBrowser, animated: true, completion: nil)
+        case "About SwiftFest":
+            performSegue(withIdentifier: "AboutSwiftFestSegue", sender: self)
+        default:
+            print("do nothing!")
+        }
+    }
 }
