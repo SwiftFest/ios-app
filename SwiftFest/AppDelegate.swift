283b306f534f774d8179d6bca7e2ac53d945345d
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2018-05-28 14:43:05 -0400

    Style nav and tab bars.

--- SwiftFest/AppDelegate.swift
+++ SwiftFest/AppDelegate.swift
@@ -1,3 +1,4 @@
+import BonMot
 import UIKit
 
 @UIApplicationMain
@@ -7,6 +8,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UINavigationBar.appearance().barTintColor = Color.black.color
+        UINavigationBar.appearance().titleTextAttributes = StringStyle(.color(Color.white.color)).attributes
+        UINavigationBar.appearance().isTranslucent = false
+
+        UITabBar.appearance().isTranslucent = false
 
         return true
     }
