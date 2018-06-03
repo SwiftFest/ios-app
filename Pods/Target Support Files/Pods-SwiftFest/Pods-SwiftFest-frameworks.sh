7de34c1e6bf59ae8c0ec4aa4b16eeac42f5f214b
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2018-06-03 16:46:59 -0400

    Finish removing UIGradient pod.

--- Pods/Target Support Files/Pods-SwiftFest/Pods-SwiftFest-frameworks.sh
+++ Pods/Target Support Files/Pods-SwiftFest/Pods-SwiftFest-frameworks.sh
@@ -147,14 +147,12 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Down/Down.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Pastel/Pastel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/UIGradient/UIGradient.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BonMot/BonMot.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Down/Down.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Pastel/Pastel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/UIGradient/UIGradient.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
