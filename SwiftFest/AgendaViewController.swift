01b789181dd34bc8cdb6706029f8f596e83013b9
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2018-06-09 20:09:44 -0400

    Refresh from network.

--- SwiftFest/AgendaViewController.swift
+++ SwiftFest/AgendaViewController.swift
@@ -12,6 +12,8 @@ class AgendaViewController: BaseViewController {
                                                   speakersById: AppDataController.shared.speakersById,
                                                   viewController: nil)
 
+    private var hasDoneInitialRefresh = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -50,7 +52,21 @@ class AgendaViewController: BaseViewController {
         agendaTableView.dataSource = agendaTableViewManager
         agendaTableView.delegate = agendaTableViewManager
         agendaTableViewManager.viewController = self
-        
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if !hasDoneInitialRefresh {
+            hasDoneInitialRefresh = true
+            AppDataController.shared.refreshFromNetwork { success in
+                guard success else { return }
+                self.agendaTableViewManager.update(
+                    agenda: AppDataController.shared.agenda,
+                    sessions: AppDataController.shared.sessions,
+                    speakersById: AppDataController.shared.speakersById
+                )
+            }
+        }
     }
     
     @IBAction func changeDay(_ sender: UISegmentedControl) {
@@ -73,15 +89,15 @@ class AgendaViewController: BaseViewController {
 
 extension AgendaViewController {
     
-    class TableViewManager: NSObject, UITableViewDelegate, UITableViewDataSource {
+    class TableViewManager: NSObject {
 
         var dayIndex: Int = 0
 
         var selectedSession: Session?
         
-        let agenda: Agenda
-        let sessions: [Session]
-        let speakersById: [Identifier<Speaker>: Speaker]
+        private var agenda: Agenda
+        private var sessions: [Session]
+        private var speakersById: [Identifier<Speaker>: Speaker]
 
         weak var viewController: AgendaViewController?
 
@@ -94,112 +110,133 @@ extension AgendaViewController {
             self.speakersById = speakersById
             self.viewController = viewController
         }
-        
-        private var sessionsBySection: [[Session]] {
-            
-            let currentDay = agenda.days[dayIndex]
-            
-            var sessionsBySection = [[Session]](repeating: [],
-                                                count: currentDay.timeslots.count)
-            
-            for (index, timeslot) in currentDay.timeslots.enumerated() {
-                let sessionsForSection = sessions.filter { timeslot.sessionIds.contains($0.id) && !$0.title.isEmpty }
-                sessionsBySection[index] = sessionsForSection
-            }
-            
-            return sessionsBySection
+
+        func update(agenda: Agenda, sessions: [Session], speakersById: [Identifier<Speaker>: Speaker]) {
+            self.agenda = agenda
+            self.sessions = sessions
+            self.speakersById = speakersById
+            viewController?.agendaTableView.reloadData()
         }
 
-        func numberOfSections(in tableView: UITableView) -> Int {
-            return agenda.days[dayIndex].timeslots.count
-        }
-        
-        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-            let startTime = agenda.days[dayIndex].timeslots[section].startTime
-            return DateFormatter.localizedString(from: startTime.date!,
-                                                 dateStyle: .none,
-                                                 timeStyle: .short)
-        }
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return sessionsBySection[section].count
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { //this one returns cell
-            guard !sessionsBySection.isEmpty,
-                sessionsBySection.count > indexPath.section,
-                !sessionsBySection[indexPath.section].isEmpty,
-                sessionsBySection[indexPath.section].count > indexPath.row else {
-                    return UITableViewCell()
-            }
-            
-            let session = sessionsBySection[indexPath.section][indexPath.row]
-            return buildCell(for: tableView, at: indexPath, using: session)
-            
-        }
-        
-        private func buildCell(for tableView: UITableView, at indexPath: IndexPath, using session: Session) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "SessionCell", for: indexPath) as! RibbonTableViewCell
-
-            let titleStyle = StringStyle(
-                .font(UIFont.boldSystemFont(ofSize: UIFontMetrics.default.scaledValue(for: 16))),
-                .color(Color.black.color),
-                .xmlRules([]) //Will force xml parsing and escaping
-            )
-            
-            cell.mainTextLabel.attributedText = session.title.styled(with: titleStyle)
-            
-            let timeslot = agenda.days[dayIndex].timeslots[indexPath.section]
-            cell.secondaryTextLabel.text = secondaryText(for: indexPath, using: timeslot)
-            cell.secondaryTextLabel.textColor = Color.lightOrange.color
-            cell.secondaryTextLabel.font = UIFont.systemFont(ofSize: UIFontMetrics.default.scaledValue(for: 14))
-            
-            cell.tertiaryTextLabel.text = tertiaryText(for: indexPath, using: session)
-            cell.tertiaryTextLabel.numberOfLines = 0
-            cell.tertiaryTextLabel.lineBreakMode = .byWordWrapping
-            cell.tertiaryTextLabel.font = UIFont.systemFont(ofSize: UIFontMetrics.default.scaledValue(for: 12))
-            cell.tertiaryTextLabel.textColor = Color.mediumGray.color
-
-            let speakerIds = session.speakers
-            let imagesNames = speakerIds.compactMap { speakersById[$0]?.thumbnailUrl }
-            cell.multiImageView.images = imagesNames.compactMap(UIImage.init(named:))
-
-            cell.selectionStyle = .none
-
-            cell.ribbon.backgroundColor = agenda.days[dayIndex].tracks[indexPath.row].uiColor
-            return cell
-        }
-        
-        private func secondaryText(for indexPath: IndexPath, using timeslot: Agenda.Timeslot) -> String {
-            
-            let startTime = DateFormatter.localizedString(from: timeslot.startTime.date!,
-                                                          dateStyle: .none,
-                                                          timeStyle: .short)
-            
-            let endTime = DateFormatter.localizedString(from: timeslot.endTime.date!,
-                                                        dateStyle: .none,
-                                                        timeStyle: .short)
-            let duration = "\(startTime) - \(endTime)"
-            return duration
-        }
-        
-        private func tertiaryText(for indexPath: IndexPath, using session: Session) -> String {
-            if session.title.lowercased() == "lunch" {
-                return ""
-            }
-            
-            let locations = ["Virginia Wimberly Theater",
-                             "Nancy and Edward Roberts Studio Theater",
-                             "Carol Dean Theatre",
-                             ]
-            
-            return locations[indexPath.row]
-        }
-        
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            let sessionDetails = sessionsBySection[indexPath.section][indexPath.row]
-            selectedSession = sessionDetails
-            viewController?.performSegue(withIdentifier: "segueToDetailView", sender: self)
-        }
     }
+}
+
+private extension AgendaViewController.TableViewManager {
+
+    var sessionsBySection: [[Session]] {
+
+        let currentDay = agenda.days[dayIndex]
+
+        var sessionsBySection = [[Session]](repeating: [],
+                                            count: currentDay.timeslots.count)
+
+        for (index, timeslot) in currentDay.timeslots.enumerated() {
+            let sessionsForSection = sessions.filter { timeslot.sessionIds.contains($0.id) && !$0.title.isEmpty }
+            sessionsBySection[index] = sessionsForSection
+        }
+
+        return sessionsBySection
+    }
+
+    func tertiaryText(for indexPath: IndexPath, using session: Session) -> String {
+        if session.title.lowercased() == "lunch" {
+            return ""
+        }
+
+        let locations = [
+            "Virginia Wimberly Theater",
+            "Nancy and Edward Roberts Studio Theater",
+            "Carol Dean Theatre",
+            ]
+
+        return locations[indexPath.row]
+    }
+
+    func secondaryText(for indexPath: IndexPath, using timeslot: Agenda.Timeslot) -> String {
+
+        let startTime = DateFormatter.localizedString(from: timeslot.startTime.date!,
+                                                      dateStyle: .none,
+                                                      timeStyle: .short)
+
+        let endTime = DateFormatter.localizedString(from: timeslot.endTime.date!,
+                                                    dateStyle: .none,
+                                                    timeStyle: .short)
+        let duration = "\(startTime) - \(endTime)"
+        return duration
+    }
+
+    func buildCell(for tableView: UITableView, at indexPath: IndexPath, using session: Session) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SessionCell", for: indexPath) as! RibbonTableViewCell
+
+        let titleStyle = StringStyle(
+            .font(UIFont.boldSystemFont(ofSize: UIFontMetrics.default.scaledValue(for: 16))),
+            .color(Color.black.color),
+            .xmlRules([]) //Will force xml parsing and escaping
+        )
+
+        cell.mainTextLabel.attributedText = session.title.styled(with: titleStyle)
+
+        let timeslot = agenda.days[dayIndex].timeslots[indexPath.section]
+        cell.secondaryTextLabel.text = secondaryText(for: indexPath, using: timeslot)
+        cell.secondaryTextLabel.textColor = Color.lightOrange.color
+        cell.secondaryTextLabel.font = UIFont.systemFont(ofSize: UIFontMetrics.default.scaledValue(for: 14))
+
+        cell.tertiaryTextLabel.text = tertiaryText(for: indexPath, using: session)
+        cell.tertiaryTextLabel.numberOfLines = 0
+        cell.tertiaryTextLabel.lineBreakMode = .byWordWrapping
+        cell.tertiaryTextLabel.font = UIFont.systemFont(ofSize: UIFontMetrics.default.scaledValue(for: 12))
+        cell.tertiaryTextLabel.textColor = Color.mediumGray.color
+
+        let speakerIds = session.speakers
+        let imagesNames = speakerIds.compactMap { speakersById[$0]?.thumbnailUrl }
+        cell.multiImageView.images = imagesNames.compactMap(UIImage.init(named:))
+
+        cell.selectionStyle = .none
+
+        cell.ribbon.backgroundColor = agenda.days[dayIndex].tracks[indexPath.row].uiColor
+        return cell
+    }
+
+}
+
+extension AgendaViewController.TableViewManager: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return agenda.days[dayIndex].timeslots.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sessionsBySection[section].count
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let startTime = agenda.days[dayIndex].timeslots[section].startTime
+        return DateFormatter.localizedString(from: startTime.date!,
+                                             dateStyle: .none,
+                                             timeStyle: .short)
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { //this one returns cell
+        guard !sessionsBySection.isEmpty,
+            sessionsBySection.count > indexPath.section,
+            !sessionsBySection[indexPath.section].isEmpty,
+            sessionsBySection[indexPath.section].count > indexPath.row else {
+                return UITableViewCell()
+        }
+
+        let session = sessionsBySection[indexPath.section][indexPath.row]
+        return buildCell(for: tableView, at: indexPath, using: session)
+
+    }
+
+}
+
+extension AgendaViewController.TableViewManager: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let sessionDetails = sessionsBySection[indexPath.section][indexPath.row]
+        selectedSession = sessionDetails
+        viewController?.performSegue(withIdentifier: "segueToDetailView", sender: self)
+    }
+
 }
