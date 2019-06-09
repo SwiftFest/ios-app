30c312102b96fa4f1bf757a8492a33ea7365cbd8
Dakota Kim <dakota@cantina.co>
2019-06-09 13:34:39 -0400

    update pods

--- Pods/BonMot/Sources/UIKit/EmbeddedTransformation.swift
+++ Pods/BonMot/Sources/UIKit/EmbeddedTransformation.swift
@@ -35,8 +35,10 @@ internal enum EmbeddedTransformationHelpers {
 
     struct Key {
 
-        static let type = NSAttributedStringKey("type")
-        static let size = NSAttributedStringKey("size")
+        static let type = NSAttributedString.Key("type")
+        static let size = NSAttributedString.Key("size")
+        static let textStyle = NSAttributedString.Key("textStyle")
+        static let maxPointSize = NSAttributedString.Key("maxPointSize")
 
     }
 
