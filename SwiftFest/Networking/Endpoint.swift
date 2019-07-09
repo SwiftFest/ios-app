24cda7ecd9490aa02959eb23f34352953f7838f7
Matt Dias <matt.dias6@gmail.com>
2019-07-08 20:19:53 -0400

    moved endpoints to enum for type safety

---
+++ SwiftFest/Networking/Endpoint.swift
@@ -0,0 +1,17 @@
+//
+//  Endpoint.swift
+//  SwiftFest
+//
+//  Created by Matthew Dias on 6/30/19.
+//  Copyright © 2019 Sean Olszewski. All rights reserved.
+//
+
+import Foundation
+
+enum Endpoint: String {
+    case agenda = "schedule"
+    case sessions = "sessions"
+    case speakers = "speakers"
+    case sponsors = "sponsors"
+    case team = "team"
+}
