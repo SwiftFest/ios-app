38fdaf8ee1b0f6568cc8d7e158d8933c54b53aa0
Matt Dias <matt.dias6@gmail.com>
2019-07-13 19:52:31 -0400

    silenced warning in main

--- SwiftFest/main.swift
+++ SwiftFest/main.swift
@@ -5,4 +5,4 @@ let appDelegateClass: AnyClass = NSClassFromString("SwiftFestTests.TestAppDelega
 let args = UnsafeMutableRawPointer(CommandLine.unsafeArgv).bindMemory(to: UnsafeMutablePointer<Int8>.self,
                                                                       capacity: Int(CommandLine.argc))
 
-UIApplicationMain(CommandLine.argc, args, nil, NSStringFromClass(appDelegateClass))
+_ = UIApplicationMain(CommandLine.argc, args, nil, NSStringFromClass(appDelegateClass))
