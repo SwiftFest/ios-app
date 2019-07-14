29e2ac943ef533beb36a0baa16d6e419c99bb9d2
Matt Dias <matt.dias6@gmail.com>
2019-07-14 12:38:43 -0400

    started work getting sponsors UI ready

--- SwiftFest/SponsorsViewController.swift
+++ SwiftFest/SponsorsViewController.swift
@@ -4,9 +4,14 @@ class SponsorsViewController: BaseViewController {
 
     var sponsors: [Sponsor] = []
     @IBOutlet weak var acknowledgementLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
 
         NotificationCenter.default.addObserver(self, selector: #selector(newDataAvailable), name: AppDataController.Notifications.dataDidUpdate, object: nil)
         
@@ -30,7 +35,31 @@ private extension SponsorsViewController {
     func updateData() {
         guard let results = AppDataController.shared.sponsors else { return }
         
-        self.sponsors = results.flatMap { $0.elements }.sorted { $0.name > $1.name }
+        sponsors = results.flatMap { $0.elements }.sorted { $0.name > $1.name }
+        tableView.reloadData()
     }
     
+}
+
+extension SponsorsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard indexPath.row < sponsors.count else { return }
+        
+        let sponsor = sponsors[indexPath.row]
+        
+        guard let url = URL(string: sponsor.link),
+              UIApplication.shared.canOpenURL(url) else { return }
+        
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+}
+
+extension SponsorsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sponsors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
