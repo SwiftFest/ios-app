2222d46649f6d2278d55fa2615b4d3c8c4ec57f4
Matt Dias <matt.dias6@gmail.com>
2019-07-14 12:24:10 -0400

    added logic to fetch and sort sponsors

--- SwiftFest/SponsorsViewController.swift
+++ SwiftFest/SponsorsViewController.swift
@@ -2,12 +2,35 @@ import UIKit
 
 class SponsorsViewController: BaseViewController {
 
+    var sponsors: [Sponsor] = []
     @IBOutlet weak var acknowledgementLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(newDataAvailable), name: AppDataController.Notifications.dataDidUpdate, object: nil)
+        
+        updateData()
     }
 
+}
+
+// MARK: - Notifications
+private extension SponsorsViewController {
+    
+    @objc func newDataAvailable(_ notification: Notification) {
+        updateData()
+    }
+    
+}
+
+// MARK: - Private
+private extension SponsorsViewController {
+    
+    func updateData() {
+        guard let results = AppDataController.shared.sponsors else { return }
+        
+        self.sponsors = results.flatMap { $0.elements }.sorted { $0.name > $1.name }
+    }
+    
 }
